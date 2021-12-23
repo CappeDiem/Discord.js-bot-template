73e33f266a353dd0cb79576c54159501d1b337dc
Cappe <42583065+CappeDiem@users.noreply.github.com>
2021-12-24 00:18:17 +0200

    Remove adding a role to a new user from guildMemberAdd

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,17 +2,9 @@ module.exports = {
     name: 'ready',
     execute(member, bot) {
         //Log the newly joined member to console
-        console.log('User' + member.user.tag + ' has joined the server!');
+        console.log('User ' + member.user.tag + ' has joined the server!');
 
         //Find a channel named welcome and send a Welcome message
         bot.channels.cache.find(c => c.name === "welcome").send('Welcome '+ member.user.username)
-
-        //Find a role called Member
-        let role = member.guild.roles.cache.find(r => r.name === 'Member');
-
-        //After 10 seconds add the member role to new user
-        setTimeout(function(){
-            member.roles.add(role);
-        }, 10000);
     }
 }
