ae777756288ca10406181b9bf2ab34d495312e0e
Cappe <42583065+CappeDiem@users.noreply.github.com>
2021-12-24 00:20:48 +0200

    Event handler patch (#4)
    * Move ready event from app.js to ready.js in events folder
    
    * Move guildMemberAdd event to a file inside events folder
    
    * Add Event file loader and remove guildMemberAdd from app.js
    
    * Remove adding a role to a new user from guildMemberAdd

---
+++ events/ready.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'ready',
+    once: true,
+    execute(bot) {
+        //Log Bot's username and the amount of servers its in to console
+        console.log(`${bot.user.username} is online on ${bot.guilds.cache.size} servers!`);
+
+        //Set the Presence of the bot user
+        bot.user.setActivity("My Code", {type: "PLAYING"});
+    }
+}
