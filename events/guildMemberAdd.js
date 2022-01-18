816ea525a9e742c1c2f3af5da76a327a0d0df07b
Cappe <42583065+CappeDiem@users.noreply.github.com>
2022-01-18 13:10:33 +0200

    Find the channel from the guild where the member joined

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -5,6 +5,6 @@ module.exports = {
         console.log('User ' + member.user.tag + ' has joined the server!');
 
         //Find a channel named welcome and send a Welcome message
-        bot.channels.cache.find(c => c.name === "welcome").send('Welcome '+ member.user.username)
+        member.guild.channels.cache.find(c => c.name === "welcome").send('Welcome '+ member.user.username)
     }
 }
