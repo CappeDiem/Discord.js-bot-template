ff95f95199f8e2649354efed4fe6caf22e6abb71
Cappe <42583065+CappeDiem@users.noreply.github.com>
2022-01-18 13:10:05 +0200

    Change the name of the guildMemberAdd.js event so it works correctly

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'ready',
+    name: 'guildMemberAdd',
     execute(member, bot) {
         //Log the newly joined member to console
         console.log('User ' + member.user.tag + ' has joined the server!');
